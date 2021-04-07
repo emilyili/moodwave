59196fe7ae5f2d87eca85670fbdf53e65b3f88ce
Karen <lin.karen@columbia.edu>
2021-04-07 18:06:48 -0400

    username functionality

--- util/util.js
+++ util/util.js
@@ -1,86 +1,123 @@
-import d3 from "d3";
-var d3 = d3;
 
-function parseCSV(file) {
-  var timeParser = d3.timeParse("%d %b %Y, %H:%M");
-  var data = d3.csv(file, function (d) {
-    return {
-      timestamp: timeParser(d.utc_time),
-      artist: d.artist,
-      album: d.album,
-      song: d.track
-    };
-  });
+// import d3 from "d3";
+// var d3 = d3;
 
-  // process songs played on each date 
-  var countDate = [];
-  var prevDate = d3.timeDay(data[data.length - 1].timestamp);
-  var temp = {};
-  var songs = [];
+// var fs = require('fs');
+// const path = require('path');
+// const mysqlConnection = require('./connection');
+// const fastcsv = require('fast-csv');
 
-  data.forEach(function (d) {
-    var date = d3.timeDay(d.timestamp);
-    if (prevDate.valueOf() !== date.valueOf()) {
-      countDate.push({});
-      countDate[countDate.length - 1].date = d3.timeDay(d.timestamp);
-      countDate[countDate.length - 1].frequency = 1;
-      countDate[countDate.length - 1].topsongs = [];
+var fs = require('fs'); 
+var parse = require('csv-parse');
+var parser = parse({columns: true}, function (err, records) {
+	console.log(records);
+});
 
-      //for top songs
-      songs = countDate[countDate.length - 1].topsongs;
-
-      temp = {};
-      temp[d.song] = 1;
-      songs.push({});
-      songs[songs.length - 1].singer = d.artist;
-      songs[songs.length - 1].name = d.song;
-      songs[songs.length - 1].songFrequency = 1;
-    } else {
-      countDate[countDate.length - 1].frequency = countDate[countDate.length - 1].frequency + 1;
-
-      //for top songs
-      if (!temp[d.song]) {
-        temp[d.song] = 1;
-        songs.push({});
-        songs[songs.length - 1].singer = d.artist;
-        songs[songs.length - 1].name = d.song;
-        songs[songs.length - 1].songFrequency = 1;
-      } else {
-        songs[songs.length - 1].songFrequency = songs[songs.length - 1].songFrequency + 1;
-      }
-    }
-
-    countDate[countDate.length - 1].topsongs.sort(function (a, b) {
-      return b.songFrequency - a.songFrequency;
-    });
-
-    prevDate = date;
-  });
-
-  return countDate;
-}
-
-function join(lookupTable, mainTable, lookupKey, mainKey, select) {
-  var l = lookupTable.length,
-    m = mainTable.length,
-    lookupIndex = [],
-    output = [];
-  for (var i = 0; i < l; i++) { // loop through l items
-    var row = lookupTable[i];
-    lookupIndex[row[lookupKey]] = row; // create an index for lookup table
-  }
-  for (var j = 0; j < m; j++) { // loop through m items
-    var y = mainTable[j];
-    var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
-    output.push(select(y, x)); // select only the columns you need
-  }
-  return output;
-}
-
-parseCSV('datasets/scrobbles-akerblomman-1617639941.csv');
+fs.createReadStream(__dirname+'../../datasets/lastfm/scrobbles-mibbie99-1617637884.csv').pipe(parser);
 
 
+//Hard coded directory has been used.
+//Put your path here...
+// const currDir = path.join('../../datasets/lastfm');
 
+// console.log(currDir)
+//Function to get the filenames present
+//in the directory
+// const readdir = (dirname) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readdir(dirname, (error, filenames) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(filenames);
+//         console.log(filenames);
+//       }
+//     });
+//   });
+// };
+
+// function parseCSV(file) {
+//   var timeParser = d3.timeParse("%d %b %Y, %H:%M");
+//   var data = d3.csv(file, function (d) {
+//     console.log("hi")
+//     };
+//   });
+// for()
+// $.ajax({
+//   url: 'csv_data.csv',
+//   dataType: 'text',
+// }).done(successFunction);
+
+//   // process songs played on each date 
+//   var countDate = [];
+//   var prevDate = d3.timeDay(data[data.length - 1].timestamp);
+//   var temp = {};
+//   var songs = [];
+
+//   data.forEach(function (d) {
+//     var date = d3.timeDay(d.timestamp);
+//     if (prevDate.valueOf() !== date.valueOf()) {
+//       countDate.push({});
+//       countDate[countDate.length - 1].date = d3.timeDay(d.timestamp);
+//       countDate[countDate.length - 1].frequency = 1;
+//       countDate[countDate.length - 1].topsongs = [];
+
+//       //for top songs
+//       songs = countDate[countDate.length - 1].topsongs;
+
+//       temp = {};
+//       temp[d.song] = 1;
+//       songs.push({});
+//       songs[songs.length - 1].singer = d.artist;
+//       songs[songs.length - 1].name = d.song;
+//       songs[songs.length - 1].songFrequency = 1;
+//     } else {
+//       countDate[countDate.length - 1].frequency = countDate[countDate.length - 1].frequency + 1;
+
+//       //for top songs
+//       if (!temp[d.song]) {
+//         temp[d.song] = 1;
+//         songs.push({});
+//         songs[songs.length - 1].singer = d.artist;
+//         songs[songs.length - 1].name = d.song;
+//         songs[songs.length - 1].songFrequency = 1;
+//       } else {
+//         songs[songs.length - 1].songFrequency = songs[songs.length - 1].songFrequency + 1;
+//       }
+//     }
+
+//     countDate[countDate.length - 1].topsongs.sort(function (a, b) {
+//       return b.songFrequency - a.songFrequency;
+//     });
+
+//     prevDate = date;
+//   });
+
+//   return countDate;
+// }
+
+// function join(lookupTable, mainTable, lookupKey, mainKey, select) {
+//   var l = lookupTable.length,
+//     m = mainTable.length,
+//     lookupIndex = [],
+//     output = [];
+//   for (var i = 0; i < l; i++) { // loop through l items
+//     var row = lookupTable[i];
+//     lookupIndex[row[lookupKey]] = row; // create an index for lookup table
+//   }
+//   for (var j = 0; j < m; j++) { // loop through m items
+//     var y = mainTable[j];
+//     var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
+//     output.push(select(y, x)); // select only the columns you need
+//   }
+//   return output;
+// }
+
+// parseCSV('datasets/scrobbles-akerblomman-1617639941.csv');
+
+// function hi() {
+//     console.log("hi") 
+// }
+// console.log("hi")
 
 //dateParser = d3.timeParse("%m/%e/%Y");
-
--- util/util.py
+++ util/util.py
@@ -1,21 +1,25 @@
 import json
 import glob
+import csv
+import pandas as pd 
+import os 
+import io
+
+from datetime import datetime
+from collections import defaultdict
 
 # read csv file
-
 
 def parseCSV(file):
     username = file.split('/')[1].split('-')[1]
     writeJSON(file, username)
 
 # process the csv file
-
 # merge with mood data
-
 # write csv file to userData.js file
 
-
-def writeJSON(file, username):
+def writeJSON(fil, username):
+    path = os.path.join('datasets/lastfm', username, ".json")
     with open('userData.txt', 'w') as json_file:
         json_file.write('var = ')
         json_file.write(username)
@@ -23,13 +27,126 @@ def writeJSON(file, username):
         # json.dump(file, json_file)
         # json_file.write('\n')
 
+# def parseAll():
+#     path = "datasets/*.csv"
+#     for file in glob.glob(path):
+#         parseCSV(file)
+#         print(file)
+ 
+def dailyData(ar):
+    countDate = []
+    prevDate = datetime.strptime(ar[len(ar)-1]['utc_time'], '%Y-%m-%dT%H:%M:%SZ')
+    # print(prevDate)
+    temp = {}
+    songs = []  
+    # for i in ar:
+    #     date = ar[len(ar)-1]['utc_time']
+#   data.forEach(function(d) {
+#     var date = d3.timeDay(d.timestamp);
+#     if(prevDate.valueOf() !== date.valueOf()){
+#       countDate.push({});
+#       countDate[countDate.length-1].date = d3.timeDay(d.timestamp);
+#       countDate[countDate.length-1].frequency = 1;
+#       countDate[countDate.length-1].topsongs = [];
 
-def parseAll():
-    path = "datasets/*.csv"
-    for file in glob.glob(path):
-        parseCSV(file)
-        print(file)
+#       //for top songs
+#       songs = countDate[countDate.length-1].topsongs;
+      
+#       temp = {};
+#       temp[d.song] = 1;
+#         songs.push({});
+#         songs[songs.length-1].singer = d.artist;
+#         songs[songs.length-1].name = d.song;
+#         songs[songs.length-1].songFrequency = 1;
+#     } else {
+#       countDate[countDate.length-1].frequency = countDate[countDate.length-1].frequency + 1;
+      
+#       //for top songs
+#       if(!temp[d.song]){
+#         temp[d.song] = 1;
+#         songs.push({});
+#         songs[songs.length-1].singer = d.artist;
+#         songs[songs.length-1].name = d.song;
+#         songs[songs.length-1].songFrequency = 1;
+#       } else {
+#         songs[songs.length-1].songFrequency = songs[songs.length-1].songFrequency + 1;
+#       }
+#      }
+    
+#     countDate[countDate.length-1].topsongs.sort(function(a,b) {
+#             return b.songFrequency - a.songFrequency;
+#           })
+    
+#     // var top = countDate[countDate.length-1].topsongs;
+#     // if(top[d.song] === undefined){
+#     //   top[d.song] = 1;
+#     // } else {
+#     //   top[d.song] = top[d.song] + 1;
+#     // }
 
+#     prevDate = date;
+#   }); 
+#   return countDate;
 
-parseAll()
-parseCSV('datasets/scrobbles-akerblomman-1617639941.csv')
+# df_list = [] 
+
+folder = os.listdir('datasets/lastfm')
+
+def loadfiles(f):
+    # jsonArray = []
+    count = 0
+
+    moodpath = os.path.join('datasets/daily-mood.csv')
+
+    for filename in f:
+        
+        startpath = os.path.join('datasets/lastfm', filename)
+
+        username = startpath.split('-')[1]
+        # print("var " + username + " = ")
+        print(username.lower() + ", ")
+        
+        endpath = os.path.join('datasets/output', username + '.json')
+        dataArray = [] 
+
+        with open(startpath, 'r', encoding='utf-8') as csvf:
+            csvReader = csv.DictReader(csvf) 
+            
+            for row in csvReader: 
+                columns = defaultdict()
+
+                for (k,v) in row.items(): # go over each column name and value 
+                    if(k == 'uts' or k == 'artist_mbid' or k == 'album_mbid' or k == 'track_mbid'):
+                        continue
+                    elif(k == 'utc_time'):
+                        columns[k] = datetime.strptime(v, '%d %b %Y, %H:%M').strftime('%Y-%m-%dT%H:%M:%SZ')
+                    else:
+                        columns[k] = v
+                dataArray.append(columns)
+                # print(row)
+            # print(jsonArray)     
+
+        dailyData(dataArray)
+        # for row in jsonArray:
+
+        with open(endpath, 'w', encoding='utf-8') as jsonf: 
+            jsonString = json.dumps(dataArray, indent=4)
+            jsonf.write(jsonString)
+
+        count = count + 1
+    # for filename in f:
+    #     df = pd.read_csv(path)
+    #     # for row in df:
+    #     #     jsonArray.append(row)
+
+    #     # print(jsonArray)
+    #     df_list.append(df)
+    #     print(filename)
+
+    # data = pd.concat(df_list)
+    # data.to_csv('final.csv')  
+
+loadfiles(folder)
+
+# parseAll()
+# parseCSV('datasets/scrobbles-akerblomman-1617639941.csv')
